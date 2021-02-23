d7888e050c1cb102e980f6eae1483482be5c6b6d
HEMANT <hemant.dhiman92@gmail.com>
2021-02-23 13:20:12 +0530

    hero-detail-component completed

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css'],
+  styleUrls: ['./heroes.component.scss'],
 })
 export class HeroesComponent implements OnInit {
   //hero = 'Hero Name'
   hero: Hero = {
     id: 1,
-    name: 'Spiderman',
+    name: 'Spider-man',
   };
+
+  heroes = HEROES;
+
+  clickedHero?: Hero;
+  
+  onClick(hero: Hero): void{
+    this.clickedHero = hero;
+  }
 
   constructor() {}
 
