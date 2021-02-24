458d9fdf3a5d9087469767008ef031fda3f6e033
HEMANT <hemant.dhiman92@gmail.com>
2021-02-24 11:41:01 +0530

    Message Service is completed

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService} from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,15 +16,26 @@ export class HeroesComponent implements OnInit {
     name: 'Spider-man',
   };
 
-  heroes = HEROES;
+
+  //heroes = HEROES;
+
+  heroes?: Hero[];
 
   clickedHero?: Hero;
-  
-  onClick(hero: Hero): void{
-    this.clickedHero = hero;
+
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe((h) => this.heroes = h);
   }
 
-  constructor() {}
+  onClick(hero: Hero):void{
+    this.clickedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 }
